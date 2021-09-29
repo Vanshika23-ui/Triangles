98d5af4273bf181f6abf9fe5a3dfd55a6b1099ab
Vanshika <2019vanshika.bajaj@ves.ac.in>
2021-09-29 11:25:56 +0530

    update

---
+++ quiz.js
@@ -0,0 +1,25 @@
+ const quizForm = document.querySelector('.quiz-Form');
+ const submitAnswerBtn = document.querySelector('#submit-answer-btn')
+ const outputE1 =  document.querySelector('#output');
+
+ const correctAnswers = ["90°" , "right angled"];
+
+ function calculateScore() {
+     let score= 0;
+     let index= 0;
+    //  var formResults = new FormData(document.getElementById(quizForm));
+    var formResults = new FormData(document.getElementById('.quizForm'));
+
+
+    // const formResults = new FormData(quizForm);
+     for (let value of formResults.values()) { 
+         if(value === correctAnswers[index]) {
+             score = score+1;
+         }
+         index = index+1;
+     }
+    //  console.log(score);
+     outputE1.innerText = "Your score is " + score;
+ }
+
+ submitAnswerBtn.addEventListener('click' , calculateScore); 
